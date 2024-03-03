3fc721e72d94de5ca0feda1030cddfd7dd37263f
BEHİÇ OYTUN ŞENKUL <114110417+behicoytunsenkul@users.noreply.github.com>
2024-03-03 18:01:54 +0300

    Initial Commit

---
+++ MetaList/MetaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaListApp.swift
+//  MetaList
+//
+//  Created by Oytun Şenkul on 3.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MetaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
